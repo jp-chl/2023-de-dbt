40348164b6992de28cc9892d2a9267482aabbb74
Juan P <data.engineer.jp.riquelme@gmail.com>
2023-02-25 20:41:11 +0000

    fact_fhv_trips with no join with dim_zones

--- models/core/fact_fhv_trips.sql
+++ models/core/fact_fhv_trips.sql
@@ -20,17 +20,17 @@ select
     trips_unioned.dropOff_datetime,
 
     trips_unioned.pickup_locationid,
-    pickup_zone.borough as pickup_borough,
-    pickup_zone.zone as pickup_zone,
+    -- pickup_zone.borough as pickup_borough,
+    -- pickup_zone.zone as pickup_zone,
 
     trips_unioned.dropoff_locationid,
-    dropoff_zone.borough as dropoff_borough,
-    dropoff_zone.zone as dropoff_zone,
+    -- dropoff_zone.borough as dropoff_borough,
+    -- dropoff_zone.zone as dropoff_zone,
 
     trips_unioned.sr_flag,
     trips_unioned.affiliated_base_number
 from trips_unioned
-inner join dim_zones as pickup_zone
-on trips_unioned.pickup_locationid = pickup_zone.locationid
-inner join dim_zones as dropoff_zone
-on trips_unioned.dropoff_locationid = dropoff_zone.locationid
+-- inner join dim_zones as pickup_zone
+-- on trips_unioned.pickup_locationid = pickup_zone.locationid
+-- inner join dim_zones as dropoff_zone
+-- on trips_unioned.dropoff_locationid = dropoff_zone.locationid
